be3c8df349ab07f335563b77742b5c09c6f18654
savannahharvey <ssammonstx@gmail.com>
2025-08-05 16:44:38 -0600

    created new test file for mocks and updated pubspec

---
+++ test/mocks.dart
@@ -0,0 +1,6 @@
+import 'package:mockito/annotations.dart';
+import 'package:byui_rideshare/services/chat_service.dart';
+import 'package:byui_rideshare/services/user_service.dart';
+
+@GenerateMocks([ChatService, UserService])
+void main() {}
